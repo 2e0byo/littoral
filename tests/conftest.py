6ae7777bab6020800e010bc755b68e8ee6788fe8
John Maximilian <2e0byo@gmail.com>
2024-02-25 20:31:24 +0000

    chore: type for compare_models.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+from typing import Callable
+
 from pydantic import BaseModel
 from pytest_cases import fixture
 
@@ -6,5 +8,7 @@ def _compare_models(a: BaseModel, b: BaseModel):
     """Compare models as dicts for better diffs."""
     assert a.model_dump() == b.model_dump()
 
+
+CompareModels = Callable[[BaseModel, BaseModel], None]
 
 compare_models = fixture(lambda: _compare_models)
