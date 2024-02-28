ee8b7da03d2973bf22faf0266dcea97a53feff50
John Maximilian <2e0byo@gmail.com>
2024-02-28 22:07:15 +0100

    refactor: move auth test to test_oauth.

--- tests/test_oauth2_flow.py
+++ tests/test_oauth2_flow.py
@@ -1,7 +1,9 @@
 # ruff: noqa: E501
+import json
 from datetime import datetime, timedelta
 
-from littoral.auth.client_oauth2 import AuthenticatedUser
+from littoral.auth.client_oauth2 import AuthenticatedUser, SimpleOauthFlow
+from littoral.testing import OauthFlowFactory
 from tests.conftest import CompareModels
 
 
@@ -31,3 +33,35 @@ def test_authenticated_user_parsed_from_json(
     user = AuthenticatedUser.model_validate_json(raw)
 
     compare_models(user, expected)
+
+
+class TestOauthFlow:
+    def test_parsed_from_json(
+        self, compare_models: CompareModels, freeze_time: datetime
+    ):
+        data = json.dumps(
+            {
+                "deviceCode": "8a409920-e440-4a0f-9f53-d2d6fe166765",
+                "userCode": "JOASE",
+                "verificationUri": "link.tidal.com",
+                "verificationUriComplete": "link.tidal.com/JOASE",
+                "expiresIn": 300,
+                "interval": 2,
+            }
+        ).encode()
+        expected = SimpleOauthFlow(
+            device_code="8a409920-e440-4a0f-9f53-d2d6fe166765",
+            user_code="JOASE",
+            verification_url="https://link.tidal.com/JOASE",
+            interval=2,
+            expires_at=freeze_time + timedelta(seconds=300),
+        )
+
+        parsed = SimpleOauthFlow.model_validate_json(data)
+
+        compare_models(parsed, expected)
+
+    def test_smoketest_factory(self):
+        fake = OauthFlowFactory().build()
+
+        assert isinstance(fake, SimpleOauthFlow)
